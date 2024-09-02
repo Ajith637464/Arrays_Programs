275cf2723868d73aae61d0e41a24fdacca770538
Ajith637464 <ajithuk112233@gmail.com>
2024-09-02 09:52:08 +0530

    Add files via upload

---
+++ PalindromeOrNot.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class PalindromeOrNot
+{
+	static Scanner sc=new Scanner (System.in);
+	public static void main(String[] args) 
+	{
+		System.out.println("Enter the Number:");
+		int n=sc.nextInt();
+
+		if ()
+		{
+		}int rev=0;
+		for (int i=n;i>0 ;i/=10)
+		{
+			int rem=i%10;
+			    rev=rev*10+rem;
+		}
+		if (n==rev)
+		{
+			System.out.println("Given Number "+n+" is a palindrome number.");
+		}
+		else
+		{
+			System.out.println("Given Number "+n+" is not a palindrome number.");
+		}
+	}
+}
