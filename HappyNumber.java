d55e8ad8b7a2e39522f0cf48be7a4b854cadb2b0
Ajith637464 <ajithuk112233@gmail.com>
2024-09-02 09:22:55 +0530

    Add files via upload

---
+++ HappyNumber.java
@@ -0,0 +1,51 @@
+import java.util.*;
+class HappyNumber 
+{
+	static Scanner sc=new Scanner (System.in);
+	public static String happyNumberOrNot(int n)
+	{
+		String res="Unhappy";
+		if (n==1)
+		{
+			res="Happy";
+		}
+		else
+		{
+			while (n != 1 && n != 4)
+			{
+				int sum=0;
+				int temp=n;
+				while (temp>0)
+				{
+					sum+=(temp%10)*(temp%10);
+					temp/=10;
+				}
+				n=sum;
+				if (n==1)
+				{
+					res="Happy";
+					break;
+				}
+			}
+		}
+		return res; 
+	}
+	public static void main(String[] args) 
+	{
+		System.out.print("Enter the Number:");
+		int n=sc.nextInt();
+		
+		/*String res=happyNumberOrNot(n);
+		System.out.println("The given number "+n+" is a "+res+" number.");
+		*/
+		for (int i=1;i<n;i++ )
+		{
+			String res=happyNumberOrNot(i);
+			if (res=="Happy")
+			{
+				System.out.println(i);
+			}
+		}
+		
+	}
+}
